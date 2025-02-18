faaf094055fd43d341341c807cb7c16a42e03b7e
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2025-02-18 15:00:11 +0530

    Refactor Dating Room components to handle async params and update app links

--- src/app/dating-room/[id]/page.tsx
+++ src/app/dating-room/[id]/page.tsx
@@ -1,12 +1,16 @@
 import { Button } from "@heroui/button";
 
-export default function DatingRoom({ params }: { params: { id: string } }) {
+
+export default async function DatingRoom({ params }: {
+    params: Promise<{ id: string }>
+}) {
+    const id = (await params).id
     return (
         <div className="flex flex-col min-h-screen">
 
             <main className="flex-grow flex items-center justify-center">
                 <Button>
-                    <a href={`https://www.wyrd.live/dating-room/${params.id}/`}>Open in app</a>
+                    <a href={`https://www.wyrd.live/dating-room/${id}/`}>Open in app</a>
                 </Button>
             </main>
 
