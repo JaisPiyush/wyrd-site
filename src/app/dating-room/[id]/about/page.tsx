d757b87d99cf0065eabddb311d2d20a4032a9cda
Piyush Jaiswal <iampiyushjaiswal103@gmail.com>
2025-02-18 22:22:50 +0530

    Update Dating Room pages to handle search parameters and improve intent URL generation

--- src/app/dating-room/[id]/about/page.tsx
+++ src/app/dating-room/[id]/about/page.tsx
@@ -1,14 +1,25 @@
 
-
-export default async function DatingRoomAbout({ params }: {
-    params: Promise<{ id: string }>
+export default async function DatingRoomAbout({ params, searchParams }: {
+    params: Promise<{ id: string }>,
+    searchParams: Promise<Record<string, string | string[] | undefined>>
 }) {
     const id = (await params).id
+    const queryParams = (await searchParams);
+    const url = new URL(`intent://www.wyrd.live/dating-room/${id}/about`);
+    if (queryParams !== undefined) {
+        for (const [key, value] of Object.entries(queryParams)) {
+            if (Array.isArray(value)) {
+                url.searchParams.set(key, value.join(','));
+            } else if (value !== undefined) {
+                url.searchParams.set(key, value);
+            }
+
+        }
+    }
     return (
         <div className="flex flex-col min-h-screen">
-
             <main className="flex-grow flex items-center justify-center">
-                <a href={`intent://www.wyrd.live/#Intent;package=live.wyrd.wyrd;scheme=wyrd;action=OPEN_WYRD_PAGE;S.path=%2Fdating-room%2F${id}%2Fabout;end;`} className="outline rounded-md px-4 py-2">Open in app</a>
+                <a href={`${url.toString()}#Intent;package=live.wyrd.wyrd;scheme=wyrd;action=OPEN_WYRD_PAGE;end;`} className="outline rounded-md px-4 py-2">Open in app</a>
             </main>
 
         </div>
